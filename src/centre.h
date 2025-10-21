7c877346ade306587f4ea963c93b0a89cd0f78a8
cherifloua <loua.cherif@esprit.tn>
2025-10-21 05:09:33 -0700

    Add my project files

--- src/centre.h
+++ src/centre.h
@@ -5,6 +5,7 @@
 #define MAX_VILLE 50
 #define MAX_TELEPHONE 20
 #define MAX_EMAIL 100
+
 typedef struct {
   int id_centre;
   char nom[MAX_NOM];
@@ -15,6 +16,7 @@ typedef struct {
   char email[MAX_EMAIL];
   int actif;
 }Centre;
+
 void ajouter_centre(Centre *nouveau_centre);
 void modifier_centre(int id_centre,Centre *centre_modifie);
 void supprimer_centre(int id_centre);
@@ -22,5 +24,6 @@ Centre* rechercher_centre_par_id(int id_centre);
 centre* rechercher_centre_par_nom(const char *nom);
 void afficher_tous_centres();
 int obtenir_nombre_centres();
+
 #endif
 
